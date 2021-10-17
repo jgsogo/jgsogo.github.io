daa67408245cab2f1c4550e85671dc58c13179fd
jgsogo <jgsogo@gmail.com>
2021-10-17 16:56:10 +0200

    styling

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,10 +1,14 @@
 <template>
   <article class="flex flex-col gap-4">
-    <h1 class="text-4xl ">{{ article.title }}</h1>
+    <h1 class="text-4xl">{{ article.title }}</h1>
     <div class="flex justify-start gap-2">
       <div class="text-gray-500">{{ formatDate(article.date) }}</div>
-      <div class="px-3 py-1 text-xs text-white bg-gray-300 rounded-full" v-for="tag of article.tags" :key="tag">
-      {{ tag }}
+      <div
+        class="px-3 py-1 text-xs text-white bg-gray-300 rounded-full"
+        v-for="tag of article.tags"
+        :key="tag"
+      >
+        {{ tag }}
       </div>
     </div>
     <nuxt-content :document="article" />
@@ -42,3 +46,30 @@ export default {
   },
 };
 </script>
+
+<style>
+.nuxt-content p {
+  margin-bottom: 20px;
+}
+.nuxt-content h2 {
+  font-weight: bold;
+  font-size: 28px;
+}
+.nuxt-content h3 {
+  font-weight: bold;
+  font-size: 22px;
+}
+.icon.icon-link {
+  background-image: url("~assets/svg/icon-hashtag.svg");
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-size: 20px 20px;
+}
+.nuxt-content-highlight {
+  @apply relative;
+}
+.nuxt-content-highlight .filename {
+  @apply absolute right-0 text-gray-600 font-light z-10 mr-2 mt-1 text-sm;
+}
+</style>
