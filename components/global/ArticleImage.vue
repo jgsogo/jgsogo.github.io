d83584b0ffbc377d169a634c0dd6903d7c99db5a
jgsogo <jgsogo@gmail.com>
2021-10-18 23:55:06 +0200

    Add component to render images in articles

---
+++ components/global/ArticleImage.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <figure class="p-4">
+    <img :src="src" :alt="alt" class="flex-grow article-image" />
+    <div v-if="source" class="text-xs text-right">Source: {{ source }}</div>
+    <figcaption v-if="caption" class="p-2 text-sm text-center">
+      <i>{{ caption }}</i>
+    </figcaption>
+  </figure>
+</template>
+
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      default: () => null,
+    },
+    source: {
+      type: String,
+      default: () => null,
+    },
+  },
+};
+</script>
