191673c885227cd0a3d8e49d2c2e9f7a67b9375b
jgsogo <jgsogo@gmail.com>
2021-10-22 23:31:59 +0200

    add style to list

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -66,16 +66,30 @@ export default {
   height: 20px;
   background-size: 20px 20px;
 }
+
 .nuxt-content-highlight {
   @apply relative;
 }
+
 .nuxt-content-highlight .filename {
   @apply absolute right-0 text-gray-600 font-light z-10 mr-2 mt-1 text-sm;
 }
+
 .nuxt-content p code {
   @apply bg-gray-100 !important;
 }
+
+.nuxt-content ul code {
+  @apply bg-gray-100 !important;
+}
+
 .nuxt-content p a {
   @apply text-red-500 hover:underline !important;
 }
+
+ul {
+  padding-left: 2rem;
+  list-style-type: square;
+}
+
 </style>
